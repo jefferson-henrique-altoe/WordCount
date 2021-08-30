8fdb7a10b439be12cdba61a5a8d4fa0610db02b0
jefferson-henrique-altoe <79026768+jefferson-henrique-altoe@users.noreply.github.com>
2021-08-30 17:40:30 -0300

    Add files via upload

---
+++ wc_app.py
@@ -0,0 +1,29 @@
+from pyspark import SparkConf, SparkContext
+from operator import add
+
+
+APP_NAME = "My WordCount Spark Application"
+FILE_NAME = "sherlock.txt"
+
+
+def tokenize(text):
+	return text.split()
+
+
+def main(sc):
+	text = sc.textFile(FILE_NAME)
+	words = text.flatMap(tokenize)
+	wc = words.map(lambda x: (x,1))
+	counts = wc.reduceByKey(add)
+	counts.saveAsTextFile("wc")
+
+
+if __name__ == "__main__":
+	conf = SparkConf().setAppName(APP_NAME)
+	conf = conf.setMaster("local[*]")
+	sc = SparkContext(conf=conf)
+	main(sc)
+
+
+# livro 'Analítica de dados com Hadoop', pág. 114
+# to run: spark-submit wc_app.py
